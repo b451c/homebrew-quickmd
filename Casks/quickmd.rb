61e5de584fa5dc364f9e5f70abdc851c6c6b4967
Bartosz Sroczynski <b.sroczynski@pm.me>
2026-04-03 15:08:08 +0200

    Update SHA for signed+notarized build

--- Casks/quickmd.rb
+++ Casks/quickmd.rb
@@ -1,6 +1,6 @@
 cask "quickmd" do
   version "1.4.1"
-  sha256 "04f3d3cf53c441cfdd0084d2a50d8343effd272b4fcc86ac6f886939861e186f"
+  sha256 "429e0ca89be600d1a1e9c2a8217bfee61b1e4ade5c0f614fb64ea156c4f1c1a6"
 
   url "https://github.com/b451c/quickmd/releases/download/v#{version}/QuickMD-v#{version}.zip",
       verified: "github.com/b451c/quickmd/"
